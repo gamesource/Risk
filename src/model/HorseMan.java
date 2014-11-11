3b5d4df8ca3d04f50b43c26ee149bea1451c7048
ucesur <ucesur@gmail.com>
2014-11-11 23:31:31 +0200

    HorseMan and Cannon are added

---
+++ src/model/HorseMan.java
@@ -0,0 +1,13 @@
+package model;
+
+import interfaces.Soldier;
+
+public class HorseMan implements Soldier {
+
+	@Override
+	public int getSoldierStrength() {
+		// TODO Auto-generated method stub
+		return 5;
+	}
+
+}
